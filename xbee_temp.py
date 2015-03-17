76d24d9021a0b43fcdd46598e3dae91fb2be7a7d
Mustafa Buda <mustafabuda@gmail.com>
2015-03-17 18:08:04 +0000

    init

---
+++ xbee_temp.py
@@ -0,0 +1,22 @@
+import serial, time, datetime, sys
+from xbee import ZigBee
+
+# WINDOWS => e.g COM3
+# MAC => e.g /dev/tty.usbserial-A702NXQX
+SERIAL_PORT = "/dev/tty.usbserial-A702NXQX"
+
+BAUD_RATE = 9600
+
+ser = serial.Serial(SERIAL_PORT, BAUD_RATE)
+
+xbee = ZigBee(ser)
+
+print 'TEST 1-2 1-2'
+while True:
+	try:
+		response = xbee.wait_read_frame()
+		print response
+	except KeyboardInterrupt:
+		break
+
+ser.close()
