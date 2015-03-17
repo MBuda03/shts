baf080efe18f1a47e405ddf5b82246b4b75c23e7
Mustafa Buda <mustafabuda@gmail.com>
2015-03-17 22:45:14 +0000

    Now printing temperature correctly with a timestamp HOWEVER TIME STAMP NEEDS TO BE FIXED

--- xbee_temp.py
+++ xbee_temp.py
@@ -1,39 +1,52 @@
-import serial, time, datetime, sys
+import serial, time, datetime, sys, sqlite3
 from xbee import ZigBee
-
-
-# WINDOWS => e.g COM3
-# MAC => e.g /dev/tty.usbserial-A702NXQX
-SERIAL_PORT = "/dev/tty.usbserial-A702NXQX"
-
-BAUD_RATE = 9600
-
-ser = serial.Serial(SERIAL_PORT, BAUD_RATE)
-
-xbee = ZigBee(ser)
-
-print 'TEST 1-2 1-2'
-while True:
-	try:
-		response = xbee.wait_read_frame()
-		print response
-
-		# Turn response into human readible
-		
-		# Add time stamps
-
-		# Add the values into sqlite/csv as
-		# Device ID, Time, Degree in C
-
-	except KeyboardInterrupt:
-		break
-
-ser.close()
 
 # Convert the value to Degrees 
 def reading_To_Degrees_C(reading):
-	print "converting data into Degrees C"
+	
+	readings = []
+	for item in reading:
+		readings.append(item.get('adc-0'))
+
+	# Averaging voltages since we are getting few samples
+	voltage_average = sum(readings)/float(len(readings))
+
+	# Converting Voltage to Temperature C. 
+	# (( VOLTAGE * 3.2258 ) - 500 ) / 10.0
+	# This only works if the sensor is connected to 3.3V out
+
+	temperature_C = (((voltage_average/1023) * 1200 ) - 500) / 10
+
+	return temperature_C
 	
 # Save Data
 def save_Data():
 	print "saving data into sql database"
+
+# WINDOWS => e.g COM3
+# MAC => e.g /dev/tty.usbserial-A702NXQX
+SERIAL_PORT = "/dev/tty.usbserial-A702NXQX"
+BAUD_RATE = 9600
+
+serial_Info = serial.Serial(SERIAL_PORT, BAUD_RATE)
+xbee = ZigBee(serial_Info)
+
+# Database
+connection = sqlite3.connect('temperature.db')
+cursor = connection.cursor()
+
+while True:
+	try:
+		response = xbee.wait_read_frame()
+		#print response
+
+		# Turn response into human readible
+		temp_C = reading_To_Degrees_C(response['samples'])
+
+		# Print the temp with timestamp
+		print  "Time: {0}, Temperature: {1}".format(int(time.time()), temp_C)
+
+	except KeyboardInterrupt:
+		break
+
+serial_Info.close()
